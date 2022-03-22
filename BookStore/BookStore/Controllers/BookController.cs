57d8205c02f2da15d052aa68b43e9eaad61c6f03
Shubham Jagtap <jagtapshubham777@gmail.com>
2022-03-22 14:56:36 +0530

    Created new Book Controller

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBook()
+        {
+            return "All Books";
+        }
+        public string GetBook(int id)
+        {
+            return $"Ramayana Book with id ={id}" ;
+        }
+        public string SearchBook(string BookName,string AuthorName)
+        {
+            return $"Book with Name ={BookName} & Book with author = {AuthorName}";
+            //https://localhost:44361/book/SearchBook?BookName=bhagat&AuthorName=koshari
+        }
+    }
+}
